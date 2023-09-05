f5aa79d01204e3f2d8b9550c460cfa5779b6c823
KMS9612 <kimdev9612@gmail.com>
2023-09-05 23:22:15 +0900

    ./crypto 삭제

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const usersRouter = require("./routes/userRoutes");
 const tokenRouter = require("./routes/tokenRoutes");
 const exerciseRouter = require("./routes/exerciseRoutes");
 require("dotenv").config();
-// require("./crypto.js");
 const app = express();
 const port = 8080;
 const swaggerUi = require("swagger-ui-express");
